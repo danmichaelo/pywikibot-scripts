3a1c1d20b9b25dbe0828872654fdce21b654f860
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-02-25 22:29:53 +0100

    Check if sitelinks have been moved, print warning if not
    Also, improve logging and keep a status file to keep track of how far
    we have processed in the log, so we can continue where we left off next
    timne.

--- update_wd_labels_from_move_log.py
+++ update_wd_labels_from_move_log.py
@@ -2,6 +2,7 @@
 # https://www.wikidata.org/wiki/Wikidata:Requests_for_permissions/Bot/DanmicholoBot_8
 
 import re
+import os
 import logging
 import pywikibot
 from prompter import prompt, yesno
@@ -39,10 +40,37 @@ lang = configs[use_config]['lang']
 dont_introduce = configs[use_config]['dont_introduce']
 dont_introduce_regexp = re.compile('(?:' + '|'.join(dont_introduce).replace('(', '\(') + ')')
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+# set up logging to file
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(module)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='update_wd_labels_from_move.log',
+                    filemode='a')
 log = logging.getLogger()
+
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)s %(levelname)-8s %(message)s')
+console_handler.setFormatter(formatter)
+log.addHandler(console_handler)
+
+# define a Handler which writes WARNING messages or higher to a separate file
+warning_handler = logging.FileHandler('update_wd_labels_from_move.warnings.log')
+warning_handler.setLevel(logging.WARNING)
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+warning_handler.setFormatter(formatter)
+log.addHandler(warning_handler)
+
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('oauthlib').setLevel(logging.WARNING)
+logging.getLogger('pywiki').setLevel(logging.WARNING)
+logging.getLogger('requests_oauthlib').setLevel(logging.WARNING)
+# for key in logging.Logger.manager.loggerDict:
+#     print(key)
+
+# --------------------------------------------------------------------------------------------
 
 site = pywikibot.Site(siteid, 'wikipedia')
 wdsite = pywikibot.Site('wikidata', 'wikidata')
@@ -78,26 +106,85 @@ def set_label(item, label, add_alias=True):
         item.editLabels({lang: label}, summary='Changing label to reflect page move on %swiki' % siteid)
         if add_alias and current_label not in item.aliases.get(lang, []):
             item.aliases[lang] = item.aliases.get(lang, []) + [current_label]
-            item.editAliases(item.aliases)
+            item.editAliases(item.aliases, summary='Adding the former label as alias')
 
 
-gen = pagegenerators.LogeventsPageGenerator(site=site, logtype='move', total='500', namespace=[0])
-for source in pagegenerators.PreloadingGenerator(gen, groupsize=50):
-    if not source.isRedirectPage():
+# for source in pagegenerators.PreloadingGenerator(gen, groupsize=50):
+
+
+def logentries_filtered(site, start_at):
+    for log_entry in site.logevents(total=500, logtype='move', namespace=0, reverse=True, start=start_at):
+        source = log_entry.page()
+        target = log_entry.target_page
+        ts = log_entry.timestamp().isoformat()
+
+        log.debug('Processing log entry <%s "%s" → "%s">', ts, source.title(), target.title())
+
+        if log_entry.target_ns.id != 0:
+            log.info('Page "%s" moved out of article namespace to "%s". Skipping', source.title(), target.title())
+            continue
+
+        if not source.exists():
+            log.info('Source page no longer exists')
+        elif not source.isRedirectPage():
+            log.info('Source page "%s" exists, but is not a redirect page! Skipping', source.title())
+            continue
+
+        if not target.exists():
+            log.info('Target page "%s" does not exist anymore. Skipping', target.title())
+            continue
+        elif target.isRedirectPage():
+            log.info('Target page "%s" is a redirect page! Skipping', target.title())
+            continue
+
+        yield log_entry
+
+
+def get_wikidata_item(page):
+    try:
+        return page.data_item()
+    except NoPage:
+        return None
+
+
+start_at_ts = '2018-01-01T00:00:00'
+if os.path.isfile('status.%s.txt' % use_config):
+    with open('status.%s.txt' % use_config) as fp:
+        start_at_ts = fp.read()
+
+
+if start_at_ts is not None:
+    logging.info('Starting from %s', start_at_ts)
+
+
+for log_entry in logentries_filtered(site, start_at=start_at_ts):
+    ts = log_entry.timestamp().isoformat()
+
+    source = log_entry.page()
+    target = log_entry.target_page
+
+    source_item = get_wikidata_item(source)
+    item = get_wikidata_item(target)
+
+    source_title = source.title()  # works for non-existing pages too
+    target_title = target.title()
+
+    if source_item is not None and item is None:
+        log.warning('Sitelink has not been moved from "%s" to "%s"', source_title, target_title)
         continue
 
-    source_title = source.title()
-    target = source.getRedirectTarget()
-    target_title = target.title()
+    if source_item is not None:
+        log.warning('Wikidata item exists for source page "%s"', source_title)
+        continue
+
+    if item is None:
+        log.info('No wikidata item for "%s"', target_title)
+        continue
+
+    # continue
 
     redirect_titles = set([p.title() for p in target.backlinks(filterRedirects='redirects')])
     redirect_titles = [dont_introduce_regexp.split(x)[0] for x in redirect_titles]
-
-    try:
-        item = target.data_item()
-    except NoPage:
-        log.warning('No wikidata item for "%s"', target_title)
-        continue
 
     # The current label at Wikidata
     current_label = item.labels.get(lang)
@@ -105,6 +192,9 @@ for source in pagegenerators.PreloadingGenerator(gen, groupsize=50):
     if current_label is None:
         log.info('%s: ADD label "%s"', item.id, target_title)
         item.editLabels({lang: target_title}, summary='Add label from %swiki' % siteid)
+        for di in dont_introduce:
+            if di in target_title:
+                log.warning('Label includes "%s" -- please verify manually!' % di)
 
     elif current_label in redirect_titles:
         # The current Wikidata label matches one of the redirect page title.
@@ -128,3 +218,6 @@ for source in pagegenerators.PreloadingGenerator(gen, groupsize=50):
     elif current_label != target_title and current_label != lcfirst(target_title):
         log.info('Page "%s" moved to "%s". WD label is "%s". Not sure what to do.',
                  source_title, target_title, current_label)
+
+    with open('status.%s.txt' % use_config, 'w+') as fp:
+        fp.write(ts)
